cc2856f49b86773af6408a847921f1de5b586421
Fabio Vergani <fabio.vergani.1975@gmail.com>
2016-11-29 17:23:47 +0100

    Update forClass.js

--- forClass.js
+++ forClass.js
@@ -5,8 +5,15 @@ aBindedCall=(e=w.Function.prototype).bind.bind(e.call),
 O=w.Object,Create=O.create,
 aRawObj=aBindedCall(Create,null,null);
 //----------------------------------------------
-var MyClass=aRawObj({
+var myClass=aRawObj({
   property1:{value:true,writable:true},
   property2:{value:"Hello"}
 });
-console.dir(e);
+//
+var istanza1=Create(myClass);
+istanza1.counter=1;
+var istanza2=Create(myClass);
+istanza2.counter=2;
+
+console.dir(istanza1);
+console.dir(istanza2);
